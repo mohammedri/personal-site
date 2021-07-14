6e52361e349e65489855d27434a784bba76824ca
Mohammed Ridwanul Islam <mohammed.ridwanul@gmail.com>
2021-07-14 12:14:43 -0400

    Remove description

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -1,12 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
 export default props => (
   <div>
     <Link to={props.node.fields.slug}>
       <h4>{props.node.frontmatter.title || props.node.fields.slug}</h4>
     </Link>
-    <p>{props.node.frontmatter.description}</p>
+    {/* <p>{props.node.frontmatter.description}</p> */}
     <br />
   </div>
-)
+);
